4f73134f5690687564775bfb8f09d1ca7de07a30
BlackListed <yura.riddig@gmail.com>
2023-04-09 09:37:41 +0300

    Created Empty Game Loop
    Also file with enum directions

---
+++ BattleShip/Directions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleShip
+{
+    public enum Directions
+    {
+        up,
+        down,
+        left,
+        right,
+    }
+}
--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -4,9 +4,37 @@ namespace BattleShip
 {
     class Program
     {
+        bool running = true;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        private void Run()
+        {
+            Start();
+            while(running)
+            {
+                CheckInput();
+                Update();
+                Render();
+            }
+        }
+
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        private void Render()
+        {
+
+        }
+        private void CheckInput()
+        {
+            
         }
     }
 }
