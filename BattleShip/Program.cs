2c41567d58ce9879f35dd5718517a326a13a6538
BlackListed <yura.riddig@gmail.com>
2023-04-11 18:01:15 +0300

    Created Ship class with base functions

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BattleShip
 {
     class Program
     {
         bool running = true;
+
+        const int windiwWidth = 40;
+        const int windowHeight = 30;
+
+        private List<Ship> ships = new List<Ship>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.Run();
         }
         private void Run()
         {
@@ -22,11 +30,12 @@ namespace BattleShip
 
         private void Start()
         {
-
+            Console.WriteLine("Hello World!");
+            SetResolution(windiwWidth, windowHeight);
         }
         private void Update()
         {
-
+            
         }
         private void Render()
         {
@@ -35,6 +44,12 @@ namespace BattleShip
         private void CheckInput()
         {
             
+        }
+
+        private void SetResolution(int width, int height)
+        {
+            Console.WindowWidth = width;
+            Console.WindowHeight = height;
         }
     }
 }
