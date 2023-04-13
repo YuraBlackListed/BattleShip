e601af085a11730c22ea489b7d4db2f894877276
BlackListed <yura.riddig@gmail.com>
2023-04-13 17:33:54 +0300

    Basic map drawing

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -7,18 +7,19 @@ namespace BattleShip
     {
         bool running = true;
 
-        const int windiwWidth = 100;
-        const int windowHeight = 30;
+        const int windiwWidth = 50;
+        const int windowHeight = 15;
 
         const int boardSize = 10;
 
         private List<Ship> ships = new List<Ship>();
 
-        char[] alphabet = new char[]
+        private char[] alphabet = new char[]
         {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
         };
+        private char seaChar = '~';
 
         static void Main(string[] args)
         {
@@ -39,7 +40,7 @@ namespace BattleShip
         private void Start()
         {
             SetResolution(windiwWidth, windowHeight);
-            
+            Console.CursorVisible = false;
         }
         private void Update()
         {
@@ -47,7 +48,9 @@ namespace BattleShip
         }
         private void Render()
         {
-            DrawField();
+            DrawYourField(0, 0);
+            DrawEnemyField(20, 0);
+
         }
         private void CheckInput()
         {
@@ -59,24 +62,38 @@ namespace BattleShip
             Console.WindowWidth = width;
             Console.WindowHeight = height;
         }
-        private void DrawField()
+        private void DrawFieldNums(int x, int y)
         {
             for (int i = 0; i < boardSize; i++)
             {
-                Console.SetCursorPosition(i + 1, 0);
+                Console.SetCursorPosition(x + i + 1, y);
                 Console.Write(alphabet[i]);
-                Console.SetCursorPosition(0, i + 1);
+                Console.SetCursorPosition(x, y + i + 1);
                 Console.Write(i);
             }
 
         }
-        private void DrawYourField()
+        private void DrawSea(int x, int y)
         {
-
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Console.SetCursorPosition(x + i + 1, y + j + 1);
+                    Console.Write(seaChar);
+                }
+            }
         }
-        private void DrawEnemyField()
+        private void DrawYourField(int x, int y)
         {
-
+            DrawFieldNums(x, y);
+            DrawSea(x, y);
         }
+        private void DrawEnemyField(int x, int y)
+        {
+            DrawFieldNums(x, y);
+            DrawSea(x, y);
+        }
+
     }
 }
