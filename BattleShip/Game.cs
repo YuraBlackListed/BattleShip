0a5a9e063f8f616cbbaffb921030107153bd9ff0
BlackListed <yura.riddig@gmail.com>
2023-04-19 19:22:14 +0300

    Added game menu

--- BattleShip/Game.cs
+++ BattleShip/Game.cs
@@ -28,15 +28,13 @@ namespace BattleShip
 
         private void Start()
         {
+            
             running = true;
             Console.CursorVisible = false;
             SetResolution(windiwWidth, windowHeight);
             maps.Start();
 
-            player1 = new Player(maps.player2Cells, maps.player1Cells, true);
-            player2 = new Player(maps.player1Cells, maps.player2Cells, true);
-
-            player1.hisTurn = true;
+            SetGameMode(GetGameMode());
         }
         private void Update()
         {
@@ -87,6 +85,55 @@ namespace BattleShip
                 Console.Write(i);
                 Thread.Sleep(1000);
             }
+        }
+
+        private void LoadMenu()
+        {
+            Console.WriteLine("Type 1 to play PvP");
+            Console.WriteLine("Type 2 to play PvAI");
+            Console.WriteLine("Type 3 to play AIvAI");
+        }
+        private int GetGameMode()
+        {
+            LoadMenu();
+            string id = Console.ReadLine();
+
+            int value = 0;
+
+            if (int.TryParse(id, out value) && value >= 0 && value <= 9)
+            {
+                Console.SetCursorPosition(0, 14);
+                Console.Write("                             ");
+                Console.SetCursorPosition(0, 0);
+            }
+            else
+            {
+                Console.SetCursorPosition(0, 14);
+                Console.Write("Invalid coordinate, try again");
+                Console.SetCursorPosition(0, 0);
+
+                value = GetGameMode();
+            }
+            return value;
+        }
+        private void SetGameMode(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    player1 = new Player(maps.player2Cells, maps.player1Cells, false);
+                    player2 = new Player(maps.player1Cells, maps.player2Cells, false);
+                    break;
+                case 2:
+                    player1 = new Player(maps.player2Cells, maps.player1Cells, false);
+                    player2 = new Player(maps.player1Cells, maps.player2Cells, true);
+                    break;
+                case 3:
+                    player1 = new Player(maps.player2Cells, maps.player1Cells, true);
+                    player2 = new Player(maps.player1Cells, maps.player2Cells, true);
+                    break;
+            }
+            player1.hisTurn = true;
         }
     }
 }
